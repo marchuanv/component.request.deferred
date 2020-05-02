748539b7871215c4ff066f4b0b83dfff4a99192b
marchuanv <marchuanv@gmail.com>
2020-05-03 00:03:25 +0200

    fix

---
+++ component.request.deferred.js
@@ -0,0 +1,21 @@
+const utils = require("utils");
+const logging = require("logging");
+const comnponentRequest = require("component.request");
+
+module.exports = { 
+    send: ({ host, port, path, method, headers, data }) => {
+        return new Promise(async (resolve)=>{
+            const requestUrl = `${host}:${port}${path}`;
+            let results = await comnponentRequest.send({  host, port, path, method, headers, data });
+            if (results.statusCode === 202){
+                logging.write("Re-Sending Deferred Request",`sending deferred request ${requestUrl}`);
+                setTimeout(async () => {
+                    results = await module.exports.send({ host, port, path, method, headers, data });
+                    resolve(results);
+                },1000);
+            } else {
+                resolve(results);
+            }
+        });
+    }
+};
---
+++ test.js
@@ -0,0 +1,38 @@
+const componentRequestDeferred= require("./component.request.deferred.js");
+const logging = require("logging");
+logging.config(["Sending Deferred Request", "Sending Request"]);
+(async()=>{
+
+    await componentRequestDeferred.send({
+        host: "localhost", 
+        port: 5000, 
+        path: "/test", 
+        method:"POST", 
+        headers: { 
+            "Content-Type":"text/plain", 
+            username: "admin1", 
+            passphrase: "secure1",
+            fromhost: "localhost",
+            fromport: 6000
+        }, 
+        data: "Hello World From Client" 
+    });
+
+    await componentRequestDeferred.send({
+        host: "localhost", 
+        port: 5000, 
+        path: "/test", 
+        method:"POST", 
+        headers: { 
+            "Content-Type":"text/plain", 
+            username: "admin1", 
+            passphrase: "secure1",
+            fromhost: "localhost",
+            fromport: 6000
+        }, 
+        data: "Hello World From Client" 
+    });
+    
+})().catch((err)=>{
+    console.log(err);
+});
